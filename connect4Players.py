9ae57e8b85faa30d8f7021c364dbfd95f2f4a024
cmartano <dine9A5Seth>
2019-11-06 13:38:49 -0500

    players file

--- connect4Players.py
+++ connect4Players.py
@@ -3,14 +3,14 @@ class HumanPlayer:
         self.playerNum = playerNum
 
     def get_move(self, board):
+        #takes player's move input
+        #returns move as an int
         movelist= board.calc_valid_moves()
-        move = input("Enter column number:")
-
-
+        move = int(input("Enter column number:"))
         while move not in movelist:
             print("Invalid move")
-            move = input("Enter column number:")
-        return int(move)
+            move = int(input("Enter column number:"))
+        return move
 
 
 
