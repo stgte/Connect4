09ee9c9fd26caf9d61105d381c4afc0f36e6d4bf
cmartano <dine9A5Seth>
2019-11-06 13:24:49 -0500

    players file

--- connect4Players.py
+++ connect4Players.py
@@ -1,6 +1,16 @@
 class HumanPlayer:
-    def __init__(self, symbol):
-        self.symbol = symbol
+    def __init__(self, playerNum):
+        self.playerNum = playerNum
 
     def get_move(self, board):
+        movelist= board.calc_valid_moves()
+        move = input("Enter column number:")
+        if move in movelist:
+            final = int(move)
+            return final
+        else:
+            print("Invalid move")
+
+
+
 
