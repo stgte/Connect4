fb84b34f8415a87f97198d7d77adebcc2232a29f
JoshHayden <jhayden@ithaca.edu>
2019-11-09 12:29:18 -0500

    Game fixed

--- game/connect4Players.py
+++ game/connect4Players.py
@@ -5,7 +5,7 @@ class HumanPlayer:
     def get_move(self, board):
         #takes player's move input
         #returns move as an int
-        movelist= board.calc_valid_moves()
+        movelist= board.getValidMoves()
         move = int(input("Enter column number:"))
         while move not in movelist:
             print("Invalid move")
