d8ac7acdcc51004420df545e24a30babd1d79c32
Tea Mdevadze <tmdevadze@ithaca.edu>
2019-11-11 15:10:45 -0500

    fixed heuristic and compare players added

--- game/gameFile.py
+++ game/gameFile.py
@@ -12,7 +12,7 @@ class Game:
         self.player1 = player1
         self.player2 = player2
         self.show_status = show_status
-        print(self.show_status)
+        # print(self.show_status)
         self.decision_times = {self.player1.symbol: 0, self.player2.symbol: 0}
         self.playGame()
 
@@ -56,13 +56,42 @@ class Game:
         else:
             return "TIE"
 
+    def get_decision_times(self):
+        return self.decision_times
+
+def compare_players(player1, player2):
+    game_count_map = {player1.symbol: 0, player2.symbol: 0, "TIE": 0}
+    time_elapsed_map = {player1.symbol: 0, player2.symbol: 0}
+    for i in range(1, 11):
+        if i % 1 == 0:
+            # pass
+            print(i, "games finished")
+
+        # swap who goes first
+        if i % 2 == 0:
+            game = Game(player1, player2, show_status=False)
+        else:
+            game = Game(player2, player1, show_status=False)
+
+        game_count_map[game.check_winner()] += 1
+        decision_times = game.get_decision_times()
+        for symbol in decision_times:
+            time_elapsed_map[symbol] += decision_times[symbol]
+    print(game_count_map)
+    print(time_elapsed_map)
+
 def main():
-    game = Game(MinimaxPlayer("R"), RandomComputerPlayer("Y"), show_status=True)
-    game = Game(RandomComputerPlayer("R"), MinimaxPlayer("Y"), show_status=True)
-    game = Game(HumanPlayer("R"), MinimaxPlayer("Y"), show_status=True)
+    # game = Game(MinimaxPlayer("R"), MinimaxPlayer("Y"), show_status=True)
+    compare_players(AlphaBetaPlayer("R"), MinimaxPlayer("Y"))
+    compare_players(MinimaxPlayer("R"), AlphaBetaPlayer("Y"))
+    # game = Game(AlphaBetaPlayer("R"), MinimaxPlayer("Y"), show_status=True)
+    # game = Game( MinimaxPlayer("R"), AlphaBetaPlayer("Y"), show_status=True)
+    # game = Game(RandomComputerPlayer("R"), MinimaxPlayer("Y"), show_status=True)
+    # game = Game( MinimaxPlayer("R"),RandomComputerPlayer("Y"), show_status=True)
+    # game = Game(HumanPlayer("R"), MinimaxPlayer("Y"), show_status=True)
 
 
-    game = Game(HumanPlayer("R"), HumanPlayer("Y"), show_status=True)
+    # game = Game(HumanPlayer("R"), HumanPlayer("Y"), show_status=True)
     print("Finished")
 
 if __name__ == "__main__":
